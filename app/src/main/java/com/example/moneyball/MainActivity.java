2aa8d6f4ac4ba00f523b6bb7341de17c18f6fa3c
Andrew Spencer <andrewos@bu.edu>
2020-04-10 18:08:47 -0400

    Added basic group page and a button to access it from main activity

--- app/src/main/java/com/example/moneyball/MainActivity.java
+++ app/src/main/java/com/example/moneyball/MainActivity.java
@@ -13,12 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button tempButton = findViewById(R.id.button);
-        final Intent tempIntent = new Intent(MainActivity.this, HomeActivity.class);
-        tempButton.setOnClickListener(new View.OnClickListener() {
+        Button tempButtonHome = findViewById(R.id.button_home);
+        Button tempButtonGroup = findViewById(R.id.button_group);
+        final Intent tempIntentHome = new Intent(MainActivity.this,HomeActivity.class);
+        final Intent tempIntentGroup = new Intent(MainActivity.this, GroupActivity.class);
+        tempButtonHome.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(tempIntent);
+                startActivity(tempIntentHome);
+            }
+        });
+        tempButtonGroup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(tempIntentGroup);
             }
         });
     }
