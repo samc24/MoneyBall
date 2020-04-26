41af0f9f5785e1e7be843417052d61a0f361b19c
israelr51 <israelram5112@gmail.com>
2020-04-26 16:38:59 -0400

    Added chat functionality and user interface.

---
+++ app/src/main/java/com/example/moneyball/Chat.java
@@ -0,0 +1,13 @@
+package com.example.moneyball;
+
+public class Chat {
+    String groupId;
+    public Chat(String groupId){
+        this.groupId = groupId;
+    }
+    public String getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(String groupId) { this.groupId = groupId;}
+}
